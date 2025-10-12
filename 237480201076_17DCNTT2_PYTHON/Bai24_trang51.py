8c31e94bdfe50562407cc52f8cd4395729d5e7d9
khoi9 <minhkhioi9897@gmail.com>
2025-10-12 10:40:11 +0700

    Commit truoc khi pull de dong bo

--- 237480201076_17DCNTT2_PYTHON/Bai24_trang51.py
+++ 237480201076_17DCNTT2_PYTHON/Bai24_trang51.py
@@ -1,15 +1,9 @@
 n = int(input("Nhập số nguyên dương n: "))
 
-chan = 0
-le = 0
+dem = 0
 tam = n
-
 while tam > 0:
-    cs = tam % 10
-    if cs % 2 == 0:
-        chan += 1
-    else:
-        le += 1
     tam //= 10
+    dem += 1
 
-print(f"Số {n} có {chan} chữ số chẵn và {le} chữ số lẻ.")
+print(f"Số {n} có {dem} chữ số.")
